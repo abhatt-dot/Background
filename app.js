a2b382e1926870c708ed8fd15385cf9fc823f382
Avni Bhatt <71513901+abhatt-dot@users.noreply.github.com>
2021-03-18 19:27:08 +0000

    Add files via upload
    This is my background colour practice!

---
+++ app.js
@@ -0,0 +1,8 @@
+//function to change the backgroud colour
+handleClick = () => {
+  document.body.style.backgroundColor = "purple";
+};
+
+reset = () => {
+  document.body.style.backgroundColor = "white";
+};
